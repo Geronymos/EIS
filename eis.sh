a3cd05df0cf89620f69096714ca77b7c1a6e650c
Geronymos <spielerbereich@gmail.com>
2021-06-15 01:06:54 +0200

    ➕ functions + statusline 🌈

--- eis.sh
+++ eis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+filename=$1
 file=$(cat $1)
 max_lines=$(wc -l < $1)
 row=0
@@ -7,8 +7,40 @@ col=0
 show_lines=false
 cmd=""
 normal_mode=1
-color=
+lastcolor=0
 
+# echo "\ue0b0 \u00b1 \ue0a0 \u27a6 \u2718 \u26a1 \u2699"
+#       >       +-      branch ->       x   ⚡      ⚙ 
+
+# Color   FG  BG
+# -------+---+---
+# Black   30  40  
+# Red     31  41
+# Green   32  42
+# Yellow  33  43
+# Blue    34  44
+# Megenta 35  45
+# Cyan    36  46
+# White   37  47
+
+function render() {
+    lastcolor=0
+    
+    clear
+    cat $filename |
+    sed -e $(($row+1))"s/./█/$(($col+1))" | 
+    highlight -O ansi --force --syntax-by-name=$filename | 
+    nl | 
+    sed -e $(($row+1))"s/./$col/" |
+    head -$(($row+$height/2)) |
+    tail -$(($height))
+}
+
+function status() {
+    [ $lastcolor != 0 ] && printf "\e[$(($lastcolor-10));$2m\ue0b0\e[0m"
+    printf "\e[1;37;$2m $1 \e[0m"
+    lastcolor=$2
+}
 
 while read -d'' -s -n1 key
 do
@@ -47,12 +79,9 @@ do
             ;;
         esac
     fi
-    clear
-    cat $1 |
-    sed -e $(($row+1))"s/./█/$(($col+1))" | 
-    highlight -O ansi --force --syntax-by-name=$1 | 
-    nl | 
-    head -$(($row+$height/2)) |
-    tail -$(($height))
-    printf "File: $1, Key: $key, Line, Col $row, $col, Width x height $width x $height, $show_lines, $cmd"
+    render
+    status "$filename" 42
+    status "$(($row))x$col" 45
+    status "Key $key " 46
+    status " " 1
 done
